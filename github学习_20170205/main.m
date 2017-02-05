6576115a318d9d2b3f188049c75ab9cea1098837
Botin Bai <baozi88748834@qq.com>
2017-02-05 15:48:51 +0800

    Initial Commit

---
+++ github学习_20170205/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  github学习_20170205
+//
+//  Created by BotinBai on 2017/2/5.
+//  Copyright © 2017年 MIU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
